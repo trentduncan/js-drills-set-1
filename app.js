3292e5d24cc5e0acc27e04b801266e69df062e59
trentduncan <trent.dunc@gmail.com>
2018-02-19 12:40:56 -0800

    drill 5

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+
+const yearOfBirth = function(age) {
+	let yearOfBirth = 2018 - age;
+	console.log(yearOfBirth);
+
+}
+
 const whoAmI = function(name, age){
 	let yearOfBirth = 2018 - age;
 	console.log(`Hi My name is ${name} and I am ${age} years old`);
 	console.log(`I was born in ${yearOfBirth}`);
 }
 whoAmI('Trent', 25);
+
+yearOfBirth(25);
 
